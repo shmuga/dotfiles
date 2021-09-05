5147f678daa1853e92487b3b4cf22548996aa0b7
Mark Orel <mail.ormark@gmail.com>
2021-09-05 17:40:55 +0100

    Easymotion->hop, gps and treesitter config

--- nvim/lua/settings/init.lua
+++ nvim/lua/settings/init.lua
@@ -2,3 +2,5 @@ require('settings.gitsigns');
 require('settings.telescope');
 require('settings.lualine');
 require('settings.nvim-autopairs');
+require('settings.nvim-treesitter');
+require('settings.hop');
