7c2de49ec1ad514a27566c319194c37c76907a41
Mark Orel <mail.ormark@gmail.com>
2022-01-12 09:30:17 +0000

    Further optimisations + leader support

--- dactyl/config.h
+++ dactyl/config.h
@@ -6,12 +6,24 @@
 
 #define SPLIT_WPM_ENABLE
 
+// For audio support add this pin
 /* #define AUDIO_PIN B7 */
 
 // Following options are the must have but should be changed directly in the keyboard config
 // since they are already defined there
 /* #define SOFT_SERIAL_PIN D3 */
-/* #define SERIAL_USE_MULTI_TRANSACTION */
 
+// Disable some of not used software features
 #define NO_ACTION_MACRO
 #define NO_ACTION_FUNCTION
+#define NO_ACTION_ONESHOT
+
+// Reduce space by not using num/caps lock
+#undef LOCKING_SUPPORT_ENABLE
+#undef LOCKING_RESYNC_ENABLE
+
+// If you use less than 8 layers
+#define LAYER_STATE_8BIT
+
+// Leader 
+#define LEADER_TIMEOUT 300
