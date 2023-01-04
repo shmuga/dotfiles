5d5197f64f4660d1b30a064d7e6e0d492f150eec
Mark <mail.ormark@gmail.com>
2023-01-04 17:29:18 +0000

    More install tweaks

--- do-setup.sh
+++ do-setup.sh
@@ -12,15 +12,12 @@ snap install tmux --classic
 curl https://sh.rustup.rs -sSf | sh
 curl -fsSL https://fnm.vercel.app/install | bash
 source /root/.bashrc
-fish_add_path ~/.cargo/bin
-fish_add_path ~/.local/share/fnm
 cargo install ripgrep
 cargo install bat
 fnm install 18
-fnm env --use-on-cd | source
 fnm use 18
 npm install -g neovim
 ./install
-# rm -rf /home/root/
-# ln -s /root /home/root
+rm -rf /home/root
+ln -s /root /home/root
 fish
